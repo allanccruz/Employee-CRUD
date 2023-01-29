eef8b0a01cb316fa7c3e4d1ccb272c2817acddce
Allan Chaves Cruz <allan.9598@gmail.com>
2023-01-29 11:29:42 -0300

    Employee-CRUD project started

---
+++ src/main/java/com/github/allanccruz/EmployeeCRUD/EmployeeCrudApplication.java
@@ -0,0 +1,13 @@
+package com.github.allanccruz.EmployeeCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCrudApplication.class, args);
+	}
+
+}
